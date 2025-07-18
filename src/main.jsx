4be90c655498feed0d0a35e58ef29facea7cb414
loredavid <83777027+loredavid@users.noreply.github.com>
2025-07-18 14:51:41 +0000

    Aggiunta funzionalita di installazione come PWA

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,13 @@ import App from './App';
 import './index.css';
 import { AuthProvider } from "./context/AuthContext";
 
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <App />
